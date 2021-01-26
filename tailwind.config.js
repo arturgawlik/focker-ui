aa63cdc25b51c80b0509067d0d27becc85f8fe34
arturgawlik <artur.gawlik@icloud.com>
2021-01-26 19:48:02 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    prefix: '',
+    purge: {
+      content: [
+        './src/**/*.{html,ts}',
+      ]
+    },
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {
+        boxShadow: ['active'],
+        backgroundColor: ['active'],
+      },
+    },
+    plugins: [],
+};
